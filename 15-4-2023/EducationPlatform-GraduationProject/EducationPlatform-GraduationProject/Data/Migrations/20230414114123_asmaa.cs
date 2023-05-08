379f3f9bd824167bc9397cfa97553907dafbc226
Your Name <asmaahanymohamed12345@gmail.com>
2023-05-08 08:22:39 +0100

    d

---
+++ 15-4-2023/EducationPlatform-GraduationProject/EducationPlatform-GraduationProject/Data/Migrations/20230414114123_asmaa.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EducationPlatform_GraduationProject.Data.Migrations
+{
+    public partial class asmaa : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
